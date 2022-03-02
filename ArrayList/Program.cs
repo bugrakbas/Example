f67d9fdc41a32b3e7895a49d0eeafff3e4fd6028
Şamil Buğrahan Akbaş <bugrahan.akbas@detaysoft.com>
2022-03-02 09:36:56 +0300

    Array Lİst

---
+++ ArrayList/Program.cs
@@ -0,0 +1,52 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections;
+
+Console.WriteLine("Hello, World!");
+
+ArrayList liste = new ArrayList();
+liste.Add("Ali");
+liste.Add("Ayşe");
+liste.Add("Veli");
+liste.Add("Serkan");
+liste.Add(2);
+liste.Add(true);
+
+//Veri Erişim
+Console.WriteLine(liste[1]);
+
+foreach (var item in liste)
+{
+    Console.WriteLine(item);
+}
+
+//Add Range
+Console.WriteLine("********* Add Range ***********");
+//string[] renkler = {"Kırmızı","Sarı","Yeşil"};
+//liste.AddRange(renkler);
+
+List<int> sayilar = new List<int>() { 1, 8, 3, 7, 9, 92, 5 };
+liste.AddRange(sayilar);
+foreach (var item in liste)
+{
+    Console.WriteLine(item);
+}
+
+//Sort
+Console.WriteLine("----- Sort ------");
+liste.Sort();
+foreach (var item in liste)
+{
+    Console.WriteLine(item);
+}
+
+Console.WriteLine("------- Binary Search -------");
+Console.WriteLine(liste.BinarySearch(9));
+
+Console.WriteLine("------- Reverse -------");
+liste.Reverse();
+foreach (var item in liste)
+{
+    Console.WriteLine(item);
+}
+
+liste.Clear();
