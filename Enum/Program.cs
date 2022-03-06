8b95a7f47bdd743abe8739d6235a3e2422cb87b3
Şamil Buğrahan Akbaş <bugrahan.akbas@detaysoft.com>
2022-03-06 17:34:20 +0300

    Enum

---
+++ Enum/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine(Gunler.Pazar);
+Console.WriteLine((int)Gunler.Cumartesi);
+
+
+int sicaklik = 32;
+if (sicaklik <= (int)HavaDurumu.Normal)
+{
+    Console.WriteLine("Dışarıya Çıkmak için havanın biraz daha ısınmasını bekle.");
+}
+else if (sicaklik >= (int)HavaDurumu.Sicak)
+{
+    Console.WriteLine("Dışarıya çıkmak için çok sıcak bir gün.");
+}
+else if ((sicaklik >= (int)HavaDurumu.Normal) && (sicaklik < (int)HavaDurumu.CokSicak))
+{
+    Console.WriteLine("Hadi dışarıya çıkalım");
+}
+
+enum Gunler
+{
+    Pazartesi = 1,
+    Salı,
+    Çarşamba,
+    Perşembe,
+    Cuma = 23,
+    Cumartesi,
+    Pazar
+}
+
+enum HavaDurumu
+{
+    Soguk = 5,
+    Normal = 20,
+    Sicak = 25,
+    CokSicak = 30
+}
